31f27d899b252a58a2eb1658f9ab0f66a2b7b9cb
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-08-19 08:14:05 +0530

    (feature) Add actions to increment and decrement count

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import './App.css';
-import {useSelector} from "react-redux"
+import {useDispatch, useSelector} from "react-redux"
+import {decrement, increment} from "./store/counter";
+import { Button, Icon } from 'semantic-ui-react'
 
 function App() {
-  const {count} = useSelector((state: any) => state.counter)
-  return (
-    <div className="App">
-     <h1>Count: {count}</h1>
-    </div>
-  );
+    const {count} = useSelector((state: any) => state.counter)
+    const dispatch = useDispatch()
+    return (
+        <div className="App">
+            <h1>Count: {count}</h1>
+            <Button animated="fade" onClick={() => dispatch(increment())}>
+                <Button.Content visible>Increment</Button.Content>
+                <Button.Content hidden>
+                    <Icon name="add" />
+                </Button.Content>
+            </Button>
+            <Button animated="fade" onClick={() => dispatch(decrement())}>
+                <Button.Content visible>Decrement</Button.Content>
+                <Button.Content hidden>
+                    <Icon name="minus" />
+                </Button.Content>
+            </Button>
+        </div>
+    );
 }
 
 export default App;
